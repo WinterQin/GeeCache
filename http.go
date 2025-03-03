dcc2f800f19bf97fd5a2b8a32680c9791e8676b8
Winter Qin <qinwinter@qq.com>
2025-03-03 22:29:18 +0800

    day03 http server

--- http.go
+++ http.go
@@ -1,22 +1,34 @@
-package geeCache
+package geecache
 
 import (
 	"fmt"
+	"geecache/consistenthash"
+	pb "geecache/geecachepb"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
+
+	"github.com/golang/protobuf/proto"
 )
 
-const defaultBasePath = "/_geecache/"
+const (
+	defaultBasePath = "/_geecache/"
+	defaultReplicas = 50
+)
 
-// HTTPPool implements PeerPicker for a pool of HTTP peers.
+// HTTPPool implements PeerPicker for a pool of HTTP peers
 type HTTPPool struct {
-	// this peer's base URL, e.g. "https://example.net:8000"
-	self     string
-	basePath string
+	self        string                 // 记录自己的地址，包括主机名/IP和端口
+	basePath    string                 // 节点间通讯地址的前缀
+	mu          sync.Mutex             // guards peers and httpGetters
+	peers       *consistenthash.Map    // 一致性哈希算法的Map，用来根据具体的key选择节点
+	httpGetters map[string]*httpGetter // 映射远程节点与对应的httpGetter。键是远程节点的地址
 }
 
-// NewHTTPPool initializes an HTTP pool of peers.
+// NewHTTPPool initializes an HTTP pool of peers
 func NewHTTPPool(self string) *HTTPPool {
 	return &HTTPPool{
 		self:     self,
@@ -51,12 +63,83 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 获取缓存值
 	view, err := group.Get(key)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	// 使用protobuf编码
+	body, err := proto.Marshal(&pb.Response{Value: view.ByteSlice()})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Write(view.ByteSlice())
+	w.Write(body)
 }
+
+// Set updates the pool's list of peers
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+// PickPeer picks a peer according to key
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
+var _ PeerPicker = (*HTTPPool)(nil)
+
+// httpGetter implements PeerGetter
+type httpGetter struct {
+	baseURL string
+}
+
+func (h *httpGetter) Get(in *pb.Request, out *pb.Response) error {
+	u := fmt.Sprintf(
+		"%v%v/%v",
+		h.baseURL,
+		url.QueryEscape(in.GetGroup()),
+		url.QueryEscape(in.GetKey()),
+	)
+
+	res, err := http.Get(u)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("server returned: %v", res.Status)
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("reading response body: %v", err)
+	}
+
+	// 使用protobuf解码
+	if err = proto.Unmarshal(bytes, out); err != nil {
+		return fmt.Errorf("decoding response body: %v", err)
+	}
+
+	return nil
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
