428fd47898a5054ffe64c0975d5cfc24f396412d
Winter Qin <qinwinter@qq.com>
2025-03-03 22:27:32 +0800

    day01 lru fifo

--- lru/lru.go
+++ lru/lru.go
@@ -1,84 +1,84 @@
 package lru
 
-import "container/list"
+import (
+	"container/list"
+)
 
-// New 方便实例化 Cache，实现 New() 函数
+// Cache 是一个LRU缓存，它不是并发安全的
+type Cache struct {
+	maxBytes  int64                         // 允许使用的最大内存
+	nbytes    int64                         // 当前已使用的内存
+	ll        *list.List                    // 双向链表
+	cache     map[string]*list.Element      // 键是字符串，值是双向链表中对应节点的指针
+	OnEvicted func(key string, value Value) // 某条记录被移除时的回调函数
+}
+
+// entry 是双向链表节点的数据类型
+type entry struct {
+	key   string
+	value Value
+}
+
+// Value 用于计算值所占用的内存大小
+type Value interface {
+	Len() int
+}
+
+// New 创建一个新的Cache
 func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
-	c := &Cache{
+	return &Cache{
 		maxBytes:  maxBytes,
-		usedBytes: 0,
-		entryList: list.New(),
-		kpMap:     make(map[string]*list.Element),
-		onEvicted: onEvicted,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
 	}
-	return c
 }
 
-// Get 从缓存中获取一个数据
-func (c *Cache) Get(key string) (value Value, ok bool) {
-	// 从kpMap中根据key，取到链表里面的指针
-	LinkedListPoint, ok := c.kpMap[key]
-	//如果有这个key
-	if ok {
-		//将这个指针移动到队头
-		c.entryList.MoveToFront(LinkedListPoint)
-		//指针对应的是entry，包含key 和value，利于我们从map中删除对应的key
-		kvEntry := LinkedListPoint.Value.(*entry)
-		//返回value，entry.value
-		return kvEntry.value, true
-	}
-	//没有就直接返回
-	return
-}
-
-// Add 向缓存中添加一个数据
+// Add 向缓存中添加一个值
 func (c *Cache) Add(key string, value Value) {
-	// 从kpMap中根据key，取到链表里面的指针
-	LinkedListPoint, ok := c.kpMap[key]
-	// 如果有这个key，那么就将这个数据移动到队头
-	if ok {
-		c.entryList.MoveToFront(LinkedListPoint)
-		// 对数据做一个更新，包括总长度的更新，和该条数据的更新
-		kvEntry := LinkedListPoint.Value.(*entry)
-		// 新增的数据大小为，新增数据的长度减去原来数据的长度
-		c.usedBytes += int64(value.Len()) - int64(kvEntry.value.Len())
-		//更新数据
-		kvEntry.value = value
-		//
-	} else { //如果没有这个key，就添加entry数据并且移动到队头,并且使用NewPoint接收指针
-		NewPoint := c.entryList.PushFront(&entry{key, value})
-		//指针添加到kpMap中
-		c.kpMap[key] = NewPoint
-		// 更新数据总大小
-		c.usedBytes += int64(value.Len())
+	if ele, ok := c.cache[key]; ok {
+		// 如果键存在，则更新对应节点的值
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		// 如果键不存在，则向缓存中添加该值
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
 	}
-	//如果超过了设定的最大值 c.maxBytes，则移除最少访问的节点
-	for c.maxBytes != 0 && c.maxBytes < c.usedBytes {
+	// 如果超过了设定的最大内存，则移除最少访问的节点
+	for c.maxBytes != 0 && c.nbytes > c.maxBytes {
 		c.RemoveOldest()
 	}
 }
 
-// RemoveOldest 移除缓存
+// Get 从缓存中获取一个值
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		// 如果键存在，将其移动到队尾
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return nil, false
+}
+
+// RemoveOldest 移除最近最少访问的节点
 func (c *Cache) RemoveOldest() {
-	//找到队尾指针
-	LinkedListPoint := c.entryList.Back()
-	//不为空则开始移除
-	if LinkedListPoint != nil {
-		//根据entry的key，从kpMap中删除该记录
-		kvEntry := LinkedListPoint.Value.(*entry)
-		delete(c.kpMap, kvEntry.key)
-		//直接从队列中移除
-		c.entryList.Remove(LinkedListPoint)
-		//维护usedBytes大小
-		c.usedBytes -= int64(kvEntry.value.Len())
-		//如果回调函数不为空，则运行
-		if c.onEvicted != nil {
-			c.onEvicted(kvEntry.key, kvEntry.value)
+	if ele := c.ll.Back(); ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
 		}
 	}
 }
 
-// Len the number of kpMap entries
+// Len 返回缓存中的记录数
 func (c *Cache) Len() int {
-	return c.entryList.Len()
+	return c.ll.Len()
 }
